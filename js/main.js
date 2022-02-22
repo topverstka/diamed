d11e0c6e50f8fac5d4f7957d94de29da69ba89b5
finiko-izh <d.novoselov1993@gmail.com>
2022-02-22 15:36:53 +0400

    Добавление фейда слайдеру с врачами

--- js/main.js
+++ js/main.js
@@ -1100,7 +1100,7 @@ document.querySelectorAll('.our_doctors__right-slider .tabs__block').forEach(i =
     arr_variable.push('.tabs__block[data-tab-body=' + i.dataset.tabBody + ']');
 });
 $(arr_variable).each((i, el) => {
-    let $carousel = $(el).find('.carousel').flickity({ 'wrapAround': true });
+    let $carousel = $(el).find('.carousel').flickity({ 'wrapAround': true, fade: true });
 
     $(el).find('.our_doctors__arrow-next').on('click', function(e) {
         flkty.next();
